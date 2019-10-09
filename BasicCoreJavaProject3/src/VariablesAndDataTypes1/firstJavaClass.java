14e8e0490658154e68e8ed8fe8d8ef70d13e426c
tej <tejashreelinghate@gmail.com>
2019-10-09 21:52:11 +0530

    firstjavaclass

---
+++ BasicCoreJavaProject3/src/VariablesAndDataTypes1/firstJavaClass.java
@@ -0,0 +1,27 @@
+package VariablesAndDataTypes1;
+
+public class firstJavaClass {
+
+	public static void main(String[] args)
+	{
+		int a=10;                     //for integer value
+		char b = 'z';                //for  single character
+		boolean c= true;            //for boolean value
+		String Name ="TEJASHREE";  //for string values string is not a data type
+	    double d = 85.5 ;          //	for decimal values
+	
+	
+	
+    	//for other data type
+	    //for numbers
+	    
+	    byte x = 10;
+	    short y=555;
+	    long z=455;
+	    
+	    //for decimal values
+	    float f =55.55f;
+			System.out.println(z);
+	}
+
+}
