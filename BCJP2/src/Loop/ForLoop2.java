38726239d4b1e4ac674d5bd85bff86f6286b998a
tej <tejashreelinghate@gmail.com>
2019-10-09 21:48:32 +0530

    forloop2

---
+++ BCJP2/src/Loop/ForLoop2.java
@@ -0,0 +1,20 @@
+package Loop;
+
+public class ForLoop2 {
+
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=4;j<=9;j++)
+			{
+				 System.out.print(j +" ");
+				 
+			}
+			System.out.println();
+				
+		}
+      
+	}
+
+}
