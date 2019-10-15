bfe340c7ff9c2113f5b2c0b964520790ca7516d1
tej <tejashreelinghate@gmail.com>
2019-10-15 21:55:49 +0530

    SeleniumClass1

---
+++ seleniumProject/src/basicSelenium/SeleniumClass1.java
@@ -0,0 +1,56 @@
+package basicSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class SeleniumClass1 
+{
+	@Test
+	public void login() throws InterruptedException
+	{
+		System.out.println("Launch chrome browser");
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\drivers\\chromedriver.exe");
+		
+		ChromeDriver dr=new ChromeDriver();
+		dr.get("http://newtours.demoaut.com/");
+		dr.manage().window().maximize();
+		
+		WebElement username =dr.findElement(By.name("userName"));
+		//Textbox
+		username.sendKeys("mercury");
+		username.clear();
+		username.sendKeys("mercury");
+		//Thread.sleep(2000);
+		
+		dr.findElement(By.name("password")).sendKeys("mercury");
+		//button
+		dr.findElement(By.name("login")).click();
+		
+		//Radiobutton
+		System.out.println(dr.findElement(By.xpath("//input[@value='oneway']")).isSelected());
+		
+		/*
+		dr.findElement(By.xpath("//input[@value='oneway']")).click();
+		System.out.println(dr.findElement(By.xpath("//input[@value='oneway']")).isSelected());
+		
+		
+		//continuebutton
+		dr.findElement(By.name("findFlights")).click();
+		
+		dr.findElement(By.name("reserveFlights")).click();*/
+WebElement pass =dr.findElement(By.xpath("//select[@name='passCount']"));
+Select sel =new Select(pass);
+		sel.selectByIndex(2);
+		Thread.sleep(2000);
+		sel.selectByValue("2");
+		Thread.sleep(2000);
+		sel.selectByVisibleText("4");
+		
+		
+	}
+
+}
