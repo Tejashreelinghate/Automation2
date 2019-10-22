2d0ebaf2f9917425fceed723e8222c83c1a3444f
tej <tejashreelinghate@gmail.com>
2019-10-22 22:05:31 +0530

    FirstMavenClass

---
+++ mavenprojects/FirstMavenProject/src/test/java/samplePackage/FirstMavenCClass.java
@@ -0,0 +1,13 @@
+package samplePackage;
+
+import org.testng.annotations.Test;
+
+public class FirstMavenCClass {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("Testing Maven");
+	}
+
+}
